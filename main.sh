c32cf2896799fc559b128fc9eae11790fb074e30
Jacob Hummer <jcbhmr@outlook.com>
2023-07-27 01:24:33 -0500

    try npm bash override

--- main.sh
+++ main.sh
@@ -2,7 +2,10 @@
 set -e
 
 if [[ $RUNNER_OS == Windows ]]; then
+  old_script_shell=$(npm config get script-shell)
+  npm config set script-shell 'C:\Program Files\git\bin\bash.exe'
   npm install --global "code-server@$INPUT_VERSION" --unsafe-perm
+  npm config set script-shell "$old_script_shell"
 else
   curl -fsSL https://code-server.dev/install.sh | sh -s -- \
     --prefix=/usr/local --version "$INPUT_VERSION"
