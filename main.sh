8d6eed2ebe2d06cb2db54b3bf5aecf42130f1a14
Jacob Hummer <jcbhmr@outlook.com>
2023-07-27 01:30:49 -0500

    use node 16

--- main.sh
+++ main.sh
@@ -2,10 +2,15 @@
 set -e
 
 if [[ $RUNNER_OS == Windows ]]; then
+  old_node_version=$(node --version)
+  old_node_version=${old_node_version:1}
+  nvm install 16
+  nvm use 16
   old_script_shell=$(npm config get script-shell)
   npm config set script-shell 'C:\Program Files\git\bin\bash.exe'
   npm install --global "code-server@$INPUT_VERSION" --unsafe-perm
   npm config set script-shell "$old_script_shell"
+  nvm use "$old_node_version"
 else
   curl -fsSL https://code-server.dev/install.sh | sh -s -- \
     --prefix=/usr/local --version "$INPUT_VERSION"
